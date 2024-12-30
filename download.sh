c4b3b3c00f1ec011fc09460c86800ceffff158ba
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-12-30 15:49:40 +0000

    Update download.sh

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ set -ex
 code_version=$1
 code_architecture=$2
 
-curl -L -o lambda.zip "https://github.com/spacelift-io/ec2-workerpool-autoscaler/releases/download/${code_version}/ec2-workerpool-autoscaler_linux_${code_architecture}.zip"
+curl -k -L -o lambda.zip "https://github.com/spacelift-io/ec2-workerpool-autoscaler/releases/download/${code_version}/ec2-workerpool-autoscaler_linux_${code_architecture}.zip"
 
 mkdir -p lambda
 cd lambda
