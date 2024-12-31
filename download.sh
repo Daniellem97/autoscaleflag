87b419b87b4cebc6daac64cc68b9c4037ed32648
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-12-31 11:33:07 +0000

    Update download.sh

--- download.sh
+++ download.sh
@@ -8,7 +8,7 @@ code_architecture=$2
 curl -L -o lambda.zip "https://github.com/spacelift-io/ec2-workerpool-autoscaler/releases/download/${code_version}/ec2-workerpool-autoscaler_linux_${code_architecture}.zip"
 
 # Install p7zip if not already installed (Amazon Linux)
-sudo yum install -y p7zip
+yum install -y p7zip
 
 mkdir -p lambda
 cd lambda
