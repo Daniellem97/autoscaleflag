fd97e14e7dee5ab7c3cef7aaf1c5d5b614ab48ec
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-12-31 11:21:54 +0000

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,24 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env sh
 set -ex
-LOGFILE="/tmp/download-debug.log"
 
-{
-  echo "Starting at: $(date)"
-  echo "Disk space:"
-  df -h /tmp
+# Download the data.
+code_version=$1
+code_architecture=$2
 
-  echo "Downloading file..."
-  curl -L --retry 3 --retry-delay 5 -o lambda.zip "https://github.com/spacelift-io/ec2-workerpool-autoscaler/releases/download/v1.0.1/ec2-workerpool-autoscaler_linux_amd64.zip"
+curl -L -o lambda.zip "https://github.com/spacelift-io/ec2-workerpool-autoscaler/releases/download/${code_version}/ec2-workerpool-autoscaler_linux_${code_architecture}.zip"
 
-  echo "File details:"
-  ls -lh lambda.zip
-  sha256sum lambda.zip
-
-  echo "Testing ZIP integrity:"
-  unzip -t lambda.zip || { echo "ZIP file integrity check failed"; exit 1; }
-
-  echo "Extracting files..."
-  mkdir -p lambda
-  cd lambda
-  unzip -o ../lambda.zip || { echo "Unzip failed"; exit 1; }
-} &>> $LOGFILE
+mkdir -p lambda
+cd lambda
+unzip -o ../lambda.zip
+rm ../lambda.zip
